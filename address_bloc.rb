c982947ef3713bda62b44bac9a3c956430ef1320
Jason Clegg <jgcdeveloper@gmail.com>
2016-11-14 23:14:34 +0000

    Add Menu Controller
    Created a bare bones menu controller that will print a menu to screen
    and ask for user input. Needs further development.

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,13 @@
+require_relative 'controllers/menu_controller'
+
+# This will create a new instance of Menu Controller locally
+menu = MenuController.new
+
+# This will clear the command line
+system "clear"
+
+# This prints a message to our screen with a newline after
 puts "Welcome to AddressBloc!"
+
+# This will call the main menu stored in our MenuController
+menu.main_menu
