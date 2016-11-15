d7a743aeaee0f77fdb1aadda90e30e7126e034a1
Jason Clegg <jgcdeveloper@gmail.com>
2016-11-15 00:01:28 +0000

    Continue Work on Menu
    Continue work on menu program. Checkpoint reached. We now can
    add an entry, and view all entries. Viewing entries now takes
    users to a submenu (functionality still to be added)

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../models/address_book'
+require 'io/console'
 
  class MenuController
    attr_reader :address_book
@@ -53,15 +54,70 @@ require_relative '../models/address_book'
    end
 
    def view_all_entries
+
+      address_book.entries.each do |entry|
+         system "clear"
+         puts entry.to_s
+         entry_submenu(entry)
+      end
+
    end
 
    def create_entry
+
+      system "clear"
+
+      puts "New AddressBloc Entry!"
+
+      print "Name: "
+      name = gets.chomp
+
+      print "Phone number: "
+      phone_number = gets.chomp
+
+      print "Email: "
+      email = gets.chomp
+
+      address_book.add_entry(name, phone_number, email)
+
+      system "clear"
+      puts "New entry created for #{name}, who can be reached at #{phone_number} or #{email}."
+
+      puts "\n\nPress any key to continue!"
+      STDIN.getch
+      system "clear"
+
    end
 
    def search_entries
+
    end
 
    def read_csv
+
+   end
+
+   def entry_submenu(entry)
+       puts "n - next entry"
+       puts "d - delete entry"
+       puts "e - edit this entry"
+       puts "m - return to main menu"
+
+       selection = STDIN.getch
+
+       case selection
+         when "n"
+         when "d"
+         when "e"
+         when "m"
+           system "clear"
+           main_menu
+         else
+           system "clear"
+           puts "#{selection} is not a valid input"
+           entry_submenu(entry)
+       end
+
    end
 
  end
